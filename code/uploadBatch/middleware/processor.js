095d98450a0cc4e190eb06c9cbb7db845346bd91
siki <srikkanthsiki@gmail.com>
2020-11-30 11:06:50 -0800

    cleasing of code

---
+++ code/uploadBatch/middleware/processor.js
@@ -0,0 +1,22 @@
+const _ = require('lodash')
+
+/*
+this is the where the actual NLP takes palce 
+tokenzise the work and split nound verb and adjective
+eg: "Anarchism is a political philosophy and movement that is sceptical of authority and rejects all involuntary, coercive forms of hierarchy. Anarchism calls for the abolition of the state which it holds to be undesirable, unnecessary and harmful."
+
+{
+       col1: 'Hello',
+       col2: 'World',
+     },
+*/
+exports.processData = async function (req, res, next) {
+console.log(req.result)
+  //  for()
+    next()
+}
+
+
+
+
+
